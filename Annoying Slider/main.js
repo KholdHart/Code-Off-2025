94a956867828932d7872dd903c0dc36a46375506
Khold Hart <greatpixelyt@gmail.com>
2025-08-22 00:34:23 +0200

    finished the slider

--- Annoying Slider/main.js
+++ Annoying Slider/main.js
@@ -1,4 +1,4 @@
-var slider = document.getAnimations("myRange");
+var slider = document.getElementById("myRange");
 var output = document.getElementById("value");
 
 output.innerHTML = slider.value;
@@ -6,3 +6,9 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
+slider.addEventListener("mousemove", function() {
+    var x = slider.value;
+    var color = "linear-gradient(90deg, rgba(255, 0, 0) " + x + "%, rgba(255, 255, 255) " + x + "%)";
+    slider.style.background = color;
+});
